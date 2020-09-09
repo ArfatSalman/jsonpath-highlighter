569c5e646cb094c03e637a64df7bba35c5913eb8
Arfat Salman <giney.paradise@gmail.com>
2020-09-09 12:04:05 +0530

    Updates

---
+++ precache-manifest.3e66922e01b13450dbf55c3ace0bb8f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ececf29d9f75d5071cafd474a3226950",
+    "url": "/jsonpath-highlighter/index.html"
+  },
+  {
+    "revision": "699b454b19a222ee6114",
+    "url": "/jsonpath-highlighter/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6f4cb9080b41c315c9ad",
+    "url": "/jsonpath-highlighter/static/css/main.f770cd61.chunk.css"
+  },
+  {
+    "revision": "699b454b19a222ee6114",
+    "url": "/jsonpath-highlighter/static/js/2.2c5ee564.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/jsonpath-highlighter/static/js/2.2c5ee564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4cb9080b41c315c9ad",
+    "url": "/jsonpath-highlighter/static/js/main.4c7bb7bc.chunk.js"
+  },
+  {
+    "revision": "6fd7e01cfb7e78807df3",
+    "url": "/jsonpath-highlighter/static/js/runtime-main.c5a19d0b.js"
+  },
+  {
+    "revision": "28c3fc39059cf6ae9db83649c7b4c8e8",
+    "url": "/jsonpath-highlighter/static/media/clipboard.28c3fc39.svg"
+  },
+  {
+    "revision": "f6cd9be569da842489c4930ed7332dbf",
+    "url": "/jsonpath-highlighter/static/media/file.f6cd9be5.svg"
+  },
+  {
+    "revision": "e8632063dc499236848a3e2e842b0712",
+    "url": "/jsonpath-highlighter/static/media/folder.e8632063.svg"
+  },
+  {
+    "revision": "a11cc29cb95a5361bd0842d03828a34c",
+    "url": "/jsonpath-highlighter/static/media/gear.a11cc29c.svg"
+  },
+  {
+    "revision": "5d9536c5d2e80f824106b35f5f4f8a36",
+    "url": "/jsonpath-highlighter/static/media/json.5d9536c5.svg"
+  }
+]);
